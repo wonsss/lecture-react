e272fb00f70378a4d91f8d2d118c90679ba70900
wonsss <bigdatajang@gmail.com>
2021-12-30 00:09:46 +0900

    feat: 추천 검색어, 최근 검색어 탭을 검색폼 아래에 추가

--- 1-vanilla/js/Controller.js
+++ 1-vanilla/js/Controller.js
@@ -1,21 +1,22 @@
 const tag = "[Controller]";
 
 export default class Controller {
-  constructor(store, { searchFormView, searchResultView }) {
+  constructor(store, { searchFormView, searchResultView, tabView }) {
     console.log(tag, "constructor");
 
     this.store = store;
 
     this.searchFormView = searchFormView;
     this.searchResultView = searchResultView;
-    // TODO
+    this.tabView = tabView;
 
     this.subscribeViewEvents();
+    this.render();
   }
 
   subscribeViewEvents() {
     this.searchFormView
-      .on("@submit", (event) => this.search(event.detail.value))
+      .on("@submit", event => this.search(event.detail.value))
       .on("@reset", () => this.reset());
   }
 
@@ -36,12 +37,12 @@ export default class Controller {
 
   render() {
     if (this.store.searchKeyword.length > 0) {
-      // TODO
+      this.tabView.hide();
       this.searchResultView.show(this.store.searchResult);
       return;
     }
 
-    // TODO
+    this.tabView.show();
     this.searchResultView.hide();
   }
 }
