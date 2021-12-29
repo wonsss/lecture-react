5bc94e1ae39847acc4090c2f46384cc1d415d7b1
wonsss <bigdatajang@gmail.com>
2021-12-29 17:55:28 +0900

    feat: 검색어를 입력하면 x버튼이 보이고, 없으면 x 버튼을 숨긴다

--- 1-vanilla/js/views/SearchFormView.js
+++ 1-vanilla/js/views/SearchFormView.js
@@ -1,4 +1,4 @@
-import { qs } from "../helpers.js";
+import { qs, on } from "../helpers.js";
 import View from "./View.js";
 
 const tag = "[SearchFormView]";
@@ -9,13 +9,26 @@ export default class SearchFormView extends View {
 
     super(qs("#search-form-view"));
 
+    this.inputElement = qs("[type=text]", this.element);
     this.resetElement = qs("[type=reset]", this.element);
 
     this.showResetButton(false);
     // TODO
+    this.bindEvent();
   }
 
   showResetButton(visible = true) {
     this.resetElement.style.display = visible ? "block" : "none";
+  }
+
+  bindEvent() {
+    on(this.inputElement, "keyup", () => this.handleKeyup());
+  }
+
+  handleKeyup() {
+    console.log(tag, "handleKeyup", this.inputElement.value);
+    const { value } = this.inputElement;
+    // 검색어를 입력하면 x버튼이 보이고, 없으면 x 버튼을 숨긴다
+    this.showResetButton(value.length > 0);
   }
 }
