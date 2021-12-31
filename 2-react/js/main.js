5ce2605a64b067f4f514c4d8d8356936297d84a7
wonsss <bigdatajang@gmail.com>
2021-12-31 16:48:45 +0900

    feat : 폼 초기화 기능 추가
    x 버튼을 클릭하거나 검색어를 삭제하면 검색 결과를 삭제한다

--- 2-react/js/main.js
+++ 2-react/js/main.js
@@ -13,11 +13,27 @@ class App extends React.Component {
     console.log("TODO: handleSubmit", this.state.searchKeyword);
   }
 
+  handleReset() {
+    // setState는 항상 비동기로 작동한다. setState호출했다하더라도 해당 변화가 다음 코드에 바로 반영되지 않고 늦게 반영된다.
+    // 그러므로 아래처럼 setState 내부에 다음 코드도 집어 넣어 작성해야 동기적으로 작동된다.
+    this.setState(
+      () => {
+        return { searchKeyword: "" };
+      },
+      () => {
+        console.log("TODO: handleReset", this.state.searchKeyword);
+      }
+    );
+  }
+
   handleChangeInput(event) {
     const searchKeyword = event.target.value;
     this.setState({
       searchKeyword,
     });
+    if (searchKeyword.length <= 0) {
+      return this.handleReset();
+    }
   }
 
   render() {
@@ -27,7 +43,10 @@ class App extends React.Component {
           <h2 className='container'>검색</h2>
         </header>
         <div className='container'>
-          <form onSubmit={event => this.handleSubmit(event)}>
+          <form
+            onSubmit={event => this.handleSubmit(event)}
+            onReset={() => this.handleReset()}
+          >
             <input
               type='text'
               placeholder='검색어를 입력하세요'
