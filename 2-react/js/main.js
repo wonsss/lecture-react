75b6311c74bd58e41cb5a0744eb7881d7eb89ee5
wonsss <bigdatajang@gmail.com>
2021-12-31 15:43:54 +0900

    feat: 입력 값을 기억하도록 state 추가

--- 2-react/js/main.js
+++ 2-react/js/main.js
@@ -1,14 +1,27 @@
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      searchKeyword: "Hello",
+    };
+  }
+
   render() {
     return (
       <>
         <header>
-          <h2 className="container">검색</h2>
+          <h2 className='container'>검색</h2>
         </header>
-        <div className="container">
+        <div className='container'>
           <form>
-            <input type="text" placeholder="검색어를 입력하세요" autoFocus />
-            <button type="reset" className="btn-reset"></button>
+            <input
+              type='text'
+              placeholder='검색어를 입력하세요'
+              autoFocus
+              value={this.state.searchKeyword}
+            />
+            <button type='reset' className='btn-reset'></button>
           </form>
         </div>
       </>
