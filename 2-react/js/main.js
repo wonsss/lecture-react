0f18d72361bea3cb25144791555e21bc9e74b24f
wonsss <bigdatajang@gmail.com>
2022-01-03 19:49:22 +0900

    feat : 번호, 추천 검색어 이름이 목록 형태로 탭 아래 위치한다

--- 2-react/js/main.js
+++ 2-react/js/main.js
@@ -19,7 +19,13 @@ class App extends React.Component {
       searchResult: [],
       submitted: false,
       selectedTab: TabType.KEYWORD,
+      keywordList: [],
     };
+  }
+
+  componentDidMount() {
+    const keywordList = store.getKeywordList();
+    this.setState({ keywordList });
   }
 
   handleSubmit(event) {
@@ -86,6 +92,19 @@ class App extends React.Component {
         <div className='empty-box'>검색 결과가 없습니다</div>
       );
 
+    const keywordList = (
+      <ul className='list'>
+        {this.state.keywordList.map((item, index) => {
+          return (
+            <li key={item.id}>
+              <span className='number'>{index + 1}</span>
+              <span>{item.keyword}</span>
+            </li>
+          );
+        })}
+      </ul>
+    );
+
     const tabs = (
       <>
         <ul className='tabs'>
@@ -101,7 +120,7 @@ class App extends React.Component {
             );
           })}
         </ul>
-        {this.state.selectedTab === TabType.KEYWORD && <>TODO: 추천 검색어</>}
+        {this.state.selectedTab === TabType.KEYWORD && keywordList}
         {this.state.selectedTab === TabType.HISTORY && <>TODO: 최근 검색어</>}
       </>
     );
