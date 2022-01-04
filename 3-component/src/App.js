2066e682647cfe7e8c92f78a8b879bfa5deebe5b
wonsss <bigdatajang@gmail.com>
2022-01-04 20:37:34 +0900

    컴포넌트로 구현하기 1 > SearchResult
    검색 결과가 검색폼 아래 위치한다. 검색 결과가 없을 경우와 있을 경우를 구분한다.
    
    x버튼을 클릭하면 검색폼이 초기화 되고, 검색 결과가 사라진다

--- 3-component/src/App.js
+++ 3-component/src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import Header from "./components/Header.js";
 import SearchForm from "./components/SearchForm.js";
+import SearchResult from "./components/SearchResult.js";
+import store from "./Store.js";
 
 export default class App extends React.Component {
   constructor() {
     super();
 
-    this.state = { searchKeyword: "" };
+    this.state = {
+      searchKeyword: "",
+      searchResult: [],
+      submitted: false,
+    };
   }
 
   handleChangeInput(searchKeyword) {
@@ -17,24 +23,41 @@ export default class App extends React.Component {
   }
 
   search(searchKeyword) {
-    console.log("TODO: search", searchKeyword);
+    console.log(searchKeyword);
+    const searchResult = store.search(searchKeyword);
+    console.log(searchResult);
+    this.setState({
+      searchResult,
+      submitted: true,
+    });
   }
 
   handleReset() {
+    this.setState({
+      searchKeyword: "",
+      submitted: false,
+      searchResult: [],
+    });
     console.log("TODO: handleReset");
   }
 
   render() {
+    const { searchKeyword, submitted, searchResult } = this.state;
+    console.log(searchKeyword);
+
     return (
       <>
         <Header title='검색' />
         <div className='container'>
           <SearchForm
-            value={this.state.searchKeyword}
+            value={searchKeyword}
             onChange={value => this.handleChangeInput(value)}
-            onSubmit={searchKeyword => this.search(searchKeyword)}
+            onSubmit={() => this.search(searchKeyword)}
             onReset={() => this.handleReset()}
           />
+          <div className='content'>
+            {submitted && <SearchResult data={searchResult} />}
+          </div>
         </div>
       </>
     );
