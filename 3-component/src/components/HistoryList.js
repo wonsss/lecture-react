c29c7c5ae10159002f4ad69e72c2591e8852e07c
wonsss <bigdatajang@gmail.com>
2022-01-04 22:28:16 +0900

    컴포넌트로 구현하기 2 > 조합: 컴포넌트 담기 - 추천검색어

--- 3-component/src/components/HistoryList.js
+++ 3-component/src/components/HistoryList.js
@@ -3,32 +3,32 @@ import List from "./List.js";
 import store from "../Store.js";
 import { formatRelativeDate } from "../helpers.js";
 
-export default class HistoryList extends List {
-  componentDidMount() {
-    this.fetch();
-  }
+// export default class HistoryList extends List {
+//   componentDidMount() {
+//     this.fetch();
+//   }
 
-  fetch() {
-    const data = store.getHistoryList();
-    this.setState({ data });
-  }
+//   fetch() {
+//     const data = store.getHistoryList();
+//     this.setState({ data });
+//   }
 
-  handleClickRemoveHistory(event, keyword) {
-    event.stopPropagation();
-    store.removeHistory(keyword);
-    this.fetch();
-  }
+//   handleClickRemoveHistory(event, keyword) {
+//     event.stopPropagation();
+//     store.removeHistory(keyword);
+//     this.fetch();
+//   }
 
-  renderItem(item) {
-    return (
-      <>
-        <span>{item.keyword}</span>
-        <span className='date'>{formatRelativeDate(item.date)}</span>
-        <button
-          className='btn-remove'
-          onClick={event => this.handleClickRemoveHistory(event, item.keyword)}
-        />
-      </>
-    );
-  }
-}
+//   renderItem(item) {
+//     return (
+//       <>
+//         <span>{item.keyword}</span>
+//         <span className='date'>{formatRelativeDate(item.date)}</span>
+//         <button
+//           className='btn-remove'
+//           onClick={event => this.handleClickRemoveHistory(event, item.keyword)}
+//         />
+//       </>
+//     );
+//   }
+// }
